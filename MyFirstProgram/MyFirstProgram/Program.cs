ed92937a74e510e82c5a23e7c7b77616b3fe135c
kazibwefrancis <francis.b.kaz@gmail.com>
2025-07-15 14:54:30 +0300

    Add project files.

---
+++ MyFirstProgram/MyFirstProgram/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+
+class Test
+{
+    static void Main()
+    {
+        Console.WriteLine("Hello, World!");
+
+
+        int x = 21;
+        int y = 22;
+        int sum = x + y;
+
+        double b = 24.6;
+        double a= 25.4;
+        double sum2 = a + b;
+
+        char symbol = '!';
+        String name = "Francis";
+
+        const int n = 2;
+        const double m = 2.2;
+
+        Console.WriteLine($"The sum of {x} and {y} is {sum} ");
+        Console.WriteLine($"The sum of {a} and {b} is {sum2}");
+        Console.WriteLine($"Your character is {symbol}");
+        Console.WriteLine($"My userName is {name} {symbol}");
+
+        //type casting = Converting a value to different data type
+        //used when we accept user input(string)
+
+        double f = 3.14;
+        int h= Convert.ToInt32( f );
+
+        int r = 100;
+        double g = Convert.ToDouble(r);
+
+        Console.WriteLine(h);
+        Console.WriteLine(h.GetType());
+        Console.WriteLine(g);
+        Console.WriteLine(g.GetType());
+
+        Console.WriteLine("Please Write your name below");
+        String name2 = Console.ReadLine();
+        Console.WriteLine(name2);
+
+        Console.WriteLine("What is your age");
+        int age = Convert.ToInt32(Console.ReadLine());
+
+        //math class
+
+        double e = 3;
+        double f2 = Math.Pow(e, 3);
+        Console.WriteLine(f2);
+
+
+        //randomizing numbers
+        Random random =new Random();
+
+        int num=random.Next(1,7);
+        double num1 = random.NextDouble();
+        Console.WriteLine(num);
+
+
+        Console.ReadKey();
+
+
+    }
+}
---
+++ calculator program/calculator program/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello welcome to my calculator program");
+        Console.WriteLine("Enter the First length of the triangle");
+        
+        double length1=double.Parse(Console.ReadLine());
+        Console.WriteLine($"The first length is :  {length1}");
+
+        Console.WriteLine("Enter the Second length of the triangle");
+        double length2=double.Parse(Console.ReadLine());
+        Console.WriteLine($"The second length is : {length2}");
+
+        double hypotenuse = Math.Sqrt((length1*length1)*(length2*length2));
+        Console.WriteLine($"The hyotenuse is : {hypotenuse}");
+
+        Console.ReadKey();
+    }
+}
